8536de36e9f46bde6f315b95449108b1f9f2ea99
XLG123 <xiaolin.guan123@gmail.com>
2023-05-11 17:02:36 -0400

    Created a ruby source code file and a test file for Recap 3

---
+++ xiaolin-guan-practice-for-ch-foundations-recap-exercises-3-main/problems_3.rb
@@ -0,0 +1,54 @@
+# require "byebug"
+
+def no_dupes?(arr)
+	
+end
+
+def no_consecutive_repeats?(arr)
+	
+end
+
+def char_indicies(str)
+	
+end
+
+def longest_streak(str)
+	
+end
+
+def bi_prime?(num)
+	
+end
+
+def vigenere_cipher(word)
+	
+end
+
+def vowel_rotate(str)
+	
+end
+
+
+# Proc Problems
+class String
+	def select(&prc)
+		
+	end
+
+	def map!(&prc)
+		
+	end
+end
+
+# Recursion Problems
+def multiply(a, b)
+	
+end
+
+def lucas_sequence(length)
+	
+end
+
+def prime_factorization(num)
+	
+end
